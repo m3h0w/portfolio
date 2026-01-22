13188a2aecb3b403f7a86f0a6a093eac7f4f1d8f
Michał Gacka <michalgacka@gmail.com>
2026-01-22 16:58:40 +0100

    www

--- src/lib/seo.js
+++ src/lib/seo.js
@@ -3,7 +3,7 @@ function getRawSiteUrl() {
     process.env.NEXT_PUBLIC_SITE_URL ||
     process.env.SITE_URL ||
     (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : undefined) ||
-    "https://michalgacka.com"
+    "https://www.michalgacka.com"
   );
 }
 
